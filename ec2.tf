3530f43bbdbb6a0ac216aa9533de5e14ed62c270
swaroopsfcrm <swaroopsfcrm@gmail.com>
2023-08-16 19:53:58 -0400

    Adding commetns to the file

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,10 @@
+##great latest AMI
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
     region = "us-east-1"
 }
-
+##create EC2
 resource "aws_instance" "ec2_instance" {
     ami = "${var.ami_id}"
     count = "${var.number_of_instances}"
